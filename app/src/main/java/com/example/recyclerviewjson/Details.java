e7de188ca3c91fbc2d2058f17be3328d495e37fe
jagmeetkaur <jagmeetrandhawa1993@gmail.com>
2019-04-08 10:48:28 -0400

    splash added

---
+++ app/src/main/java/com/example/recyclerviewjson/Details.java
@@ -0,0 +1,6 @@
+package com.example.recyclerviewjson;
+
+public class Details
+{
+
+}
---
+++ app/src/main/java/com/example/recyclerviewjson/Splash.java
@@ -0,0 +1,27 @@
+package com.example.recyclerviewjson;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Splash extends AppCompatActivity {
+
+    private static int SPLASH_TIME_OUT = 4000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent LoginIntent = new Intent(Splash.this,MainActivity.class);
+                startActivity(LoginIntent);
+                finish();
+
+            }
+        },SPLASH_TIME_OUT);
+    }
+}
